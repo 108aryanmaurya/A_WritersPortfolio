da4aa38f04c633c59e59c54b1a0d7a89766c4cc0
Aryan <108aryanmaurya@gmail.com>
2023-12-09 16:27:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <div className="bg-red-500">Hello</div>;
+}
+
+export default App;
