069235d6db4d741daaa41248a3f08a876ee460a7
Ashwin Maurya <ashwinmaurya30@gmail.com>
2023-12-10 19:57:53 +0530

    changes made

--- src/section/Home/MoreContent.jsx
+++ src/section/Home/MoreContent.jsx
@@ -25,14 +25,6 @@ const cardData = [
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni reiciendis sequi ipsam incidunt.",
   },
-  {
-    id: 1,
-    image:
-      "https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    title: "Lorem, ipsum dolor.",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni reiciendis sequi ipsam incidunt.",
-  },
 ];
 
 export default function MoreContent() {
