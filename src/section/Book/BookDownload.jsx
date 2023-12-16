e06340e1051640d46c39365bee247b052cc355e4
Aryan <108aryanmaurya@gmail.com>
2023-12-16 19:10:12 +0530

    changes mde

--- src/section/Book/BookDownload.jsx
+++ src/section/Book/BookDownload.jsx
@@ -4,8 +4,8 @@ import { Download } from "../../assets/icons";
 
 const BookDownload = () => {
   return (
-    <section className="flex justify-end mt-36 items-center relative">
-      <div className="z-10  max-sm:p-1 max-md:mt-2  text-[30px] font-[250] absolute  left-[29%] max-md:left-0  top-[40%]  max-md:m-1    w-[400px] flex justify-center items-center h-20   bg-gray-900 text-white   border-2 p-2 gap-2 uppercase max-md:gap-1 max-sm:text-[14px]   border-black">
+    <section className="flex justify-end max-md:mt-14 mt-36 items-center relative">
+      <div className="z-10  max-sm:p-1 max-md:mt-2  text-[30px] font-[250] absolute  left-[29%]  max-md:left-2  top-[40%]   max-md:m-0    w-[400px] max-md:w-auto max-md:h-auto   flex justify-center items-center h-20   bg-gray-900 text-white   border-2 p-2 gap-2 uppercase max-md:gap-1 max-sm:text-[14px]   border-black">
         <span>get a sample pdf</span>
         <img
           src={Download}
@@ -13,10 +13,10 @@ const BookDownload = () => {
           alt="dwl"
         />
       </div>
-      <div className="w-[500px] max-md:w-full  mr-12 p-22 z-30  my-20">
+      <div className="w-[500px] max-md:w-[200px] mr-12 max-md:mr-6 p-22 z-30  max-md:my-10 my-20">
         <img src={realdownload} alt="" className="" />
       </div>
-      <div className="bg-gray-300 absolute right-28 p-22 h-[100%]     w-[60%]"></div>
+      <div className="bg-gray-300  max-md:left-10 absolute right-28 p-22 h-[100%]     max-md:w-[70%]   w-[60%]"></div>
     </section>
   );
 };
--- src/section/Book/BookSynopsis.jsx
+++ src/section/Book/BookSynopsis.jsx
@@ -3,8 +3,8 @@ import { Synopsis, download } from "../../assets/images";
 
 const BookSynopsis = () => {
   return (
-    <section className="relative max-md:mt-6 flex mt-36">
-      <div className="flex justify-between  max-md:flex-col  items-center">
+    <section className="relative max-md:mt-14 flex mt-36">
+      <div className="flex justify-between max-md:justify-center  max-md:flex-col  items-center">
         <div className="w-[400px]   max-md:w-[100%]  max-md:py-0 py-20 max-md:h-[100%] max-md:bg-black  max-md:blur-[1px]   h-full ">
           <img
             src={download}
@@ -12,10 +12,10 @@ const BookSynopsis = () => {
             alt=""
           />
         </div>
-        <div className=" z-30  text-[35px]  font-bold  max-md:text-[23px] absolute  left-40 max-md:top-14  font-[100] font-CooperHevitt     border-b-[3px] tracking-widest    text-white border-white ">
+        <div className=" z-30  text-[35px]  font-bold  max-md:text-[23px] absolute  left-40 max-md:top-8  font-[100] font-CooperHevitt     border-b-[3px] tracking-widest    text-white border-white ">
           SYNOPSIS
         </div>
-        <div className="z-20 mr-72 max-md:absolute max-md:top-40 max-md:bg-opacity-[0.1]  max-md:text-white   max-md:mr-0 bg-white text-[18px]   w-[40%] max-md:w-[90%]  max-md:text-[14px] max-md:leading-[19px] font-montserrat    max-md:ml-0  max-sm:mt-0  max-md:my-0  my-20   p-10 max-md:p-3 text-justify  ">
+        <div className="z-20 mr-72 max-md:absolute max-md:top-24 max-md:bg-opacity-[0.1]  max-md:text-white   max-md:mr-0 bg-white text-[18px]   w-[40%] max-md:w-[90%]  max-md:text-[14px] max-md:leading-[19px] font-montserrat    max-md:ml-0  max-sm:mt-0  max-md:my-0  my-20   p-10 max-md:p-3 text-justify  ">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dolorum
           iure nesciunt enim dignissimos cumque voluptatum eos, possimus odio
           error perferendis nihil atque laudantium rem quos esse cupiditate
