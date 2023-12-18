6a32dd01016196cbb850a409d96a3671da09baf9
Aryan <108aryanmaurya@gmail.com>
2023-12-18 21:10:31 +0530

    changes mde

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ import SocialLinks from "./SocialLinks";
 export default function Footer() {
   return (
     <>
-      <footer className="bg-white mt-20">
+      <footer className="bg-white pt-10">
         <div className=" w-full py-6 max-lg:py-2 px-20 max-lg:px-5">
           <div className="flex max-lg:flex-col">
             <div className="max-lg:my-5 max-lg:mb-10 w-[60%] max-lg:w-full">
-              <h1 class="text-xl my-1 font-bold mb-3 max-sm:mb-0 font-CooperHevitt 2xl:text-2xl">
+              <h1 className="text-xl my-1 font-bold mb-3 max-sm:mb-0 font-CooperHevitt 2xl:text-2xl">
                 Susbcribe to our Newsletter
               </h1>
               <div className=" w-[70%] my-2 max-lg:w-[90%] max-sm:w-full">
--- src/section/Book/AboutBook.jsx
+++ src/section/Book/AboutBook.jsx
@@ -4,7 +4,7 @@ import { cover1 } from "../../assets/images";
 const AboutBook = () => {
   return (
     <>
-      <div className="font-Authorfont text-[45px] text-center max-md:my-7 max-md:text-[25px] mt-12">
+      <div className=" text-[50px] font-Gamiliademo text-center max-md:my-7 max-md:text-[25px] mt-12">
         The Two Towers
       </div>
       <div className="relative  max-lg:hidden  p-16 max-sm:p-0 max-md:mt-8  max-sm:ml-4  justify-center items-center">
@@ -24,6 +24,7 @@ const AboutBook = () => {
       <div className="bg-slate-200 -z-50  h-72 top-72 w-72 max-lg:hidden  rounded-full  absolute  right-32 "></div>
       <div className="bg-slate-200 -z-50 h-48 top-[600px] w-48 max-lg:hidden  rounded-full  absolute  right-4 "></div> */}
         <div className="text-[18px]    absolute z-20  text-justify bg-white w-[45%] max-lg:w-[80%] max-md:w-[80%] max-sm:w-[85%] max-sm:p-2 max-sm:leading-[17px] max-md:text-[16px] max-sm:text-[14px] font-montserrat   max-sm:ml-8 max-sm:mt-14  top-36 right-36 p-14  max-sm:pt-5   ">
+          <span className="text-[20px] absolute"> "</span>
           <span className="text-[35px]  ml-3 mr-1 font-serif ">L</span>
           orem ipsum dolor sit amet, consectetur adipisicing elit. Illo dolorum
           iure nesciunt enim dignissimos cumque voluptatum eos, possimus odio
@@ -32,10 +33,11 @@ const AboutBook = () => {
           error perferendis nihil atque laudantium rem quos esse cupiditate
           repellat minima. Lorem ipsum dolor sit, amet consectetur adipisicing
           elit. Fugit non, dignissimos quisquam doloremque reprehenderit quod
-          dolores iste impedit sed voluptas quasi culpa fuga delectus asperiores
+          dolores iste impedit sed voluptas quasi culpa fuga delectus
+          asperiores."
         </div>
       </div>
-      <div className="relative lg:hidden  p-16 max-sm:p-0 max-md:mt-3  max-sm:ml-4  justify-center items-center">
+      <div className="relative lg:hidden  z-50 p-16 max-sm:p-0 max-md:mt-3  max-sm:ml-4  justify-center items-center">
         <div className="float-left ">
           <img
             src={cover1}
@@ -51,7 +53,8 @@ const AboutBook = () => {
         <div className="bg-slate-200 -z-50 max-lg:hidden h-28  w-28 right-1 rounded-full  absolute  "></div>
         <div className="bg-slate-200 -z-50  h-72 top-72 w-72 max-lg:hidden  rounded-full  absolute  right-32 "></div>
         <div className="bg-slate-200 -z-50 h-48 top-[600px] w-48 max-lg:hidden  rounded-full  absolute  right-4 "></div>
-        <div className="text-[18px]  text-justify bg-white w-[60%] max-lg:w-[80%] max-md:w-[80%] max-sm:w-[85%] max-sm:p-2 max-sm:leading-[17px] max-md:text-[15px]  font-montserrat   max-sm:ml-8 max-md:mt-14  ml-24 mt-24  p-14  max-sm:pt-5   ">
+        <div className="text-[18px] -z-50 relative text-justify bg-white w-[60%] max-lg:w-[80%] max-md:w-[80%] max-sm:w-[85%] max-sm:p-2 max-sm:leading-[17px] max-md:text-[15px]  font-montserrat   max-sm:ml-8 max-md:mt-14  ml-24 mt-24  p-14  max-sm:pt-5   ">
+          <span className="text-[20px]  absolute"> "</span>
           <span className="text-[35px]  ml-3 mr-1 font-serif ">L</span>
           orem ipsum dolor sit amet, consectetur adipisicing elit. Illo dolorum
           iure nesciunt enim dignissimos cumque voluptatum eos, possimus odio
