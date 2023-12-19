c7aa1b12be0857e4f9c8745b777abd51fbb87847
Aryan <108aryanmaurya@gmail.com>
2023-12-19 09:25:16 +0530

    progress on testimonials

--- src/pages/SingleBookPage.jsx
+++ src/pages/SingleBookPage.jsx
@@ -14,8 +14,8 @@ const SingleBookPage = ({ bookdetails }) => {
       <BookHero bookdetails={bookdetails}></BookHero>
       <AboutBook></AboutBook>
       <BookSynopsis></BookSynopsis>
-      <BookDownload></BookDownload>
       <Testimonials></Testimonials>
+      <BookDownload></BookDownload>
       {/* <div className=" max-sm:p-1 max-md:mt-6  max-md:m-2   w-auto max-w-[200px] flex justify-center items-center flex-row text-black   border-2 p-2 gap-2 max-sm:text-[14px]   border-black">
         <span>GET A SAMPLE</span>
         <img src={Download} className="w-8 px-1 pl-2" alt="" />
