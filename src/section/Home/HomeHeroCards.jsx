ce0b72025d73ff9f6f9359d2654c489d50c1f07d
Ashwin Maurya <ashwinmaurya30@gmail.com>
2023-12-22 19:53:17 +0530

    changes made new

--- src/section/Home/HomeHeroCards.jsx
+++ src/section/Home/HomeHeroCards.jsx
@@ -1,19 +1,19 @@
 // HomeHeroCards.jsx
 import React from "react";
-
-const HomeHeroCards = ({ imageSrc, backgroundColor }) => {
+import "./HomeHeroCards.css";
+const HomeHeroCards = ({ story }) => {
   return (
     <div className="relative w-[1000px] max-xl:w-[900px] max-lg:w-[700px] 2xl:w-[1200px] max-sm:w-full rounded max-md:rounded-none h-full flex-shrink-0 snap-center overflow-hidden">
-      <div className="w-full h-full flex flex-row group text-center relative uppercase overflow-hidden">
+      <div className="w-full bg-black h-full flex flex-row group text-center relative uppercase overflow-hidden">
         <div
-          className={`${backgroundColor} transition-all ease-in-out duration-200 z-[1] w-[80%] h-full  [clip-path:polygon(0%_0%,65%_0%,100%_100%,0%_100%)]`}
+          className={`bg-gray-800 transition-all ease-in-out duration-200 opacity-80 z-[1] w-[80%] h-full  [clip-path:polygon(0%_0%,65%_0%,100%_100%,0%_100%)]`}
         >
           <div class="w-[77%] h-full flex justify-center items-center flex-col text-left space-y-2 p-3">
             <div className="ml-10 max-sm:ml-0">
-              <h3 class="font-black text-gray-800 md:text-3xl text-xl">
-                Lorem Ipsum dolor
+              <h3 class="font-black text-gray-100 md:text-3xl text-xl">
+                {story?.title}
               </h3>
-              <h3 class="font-black font-Authorfont tracking-wider mb-5 text-gray-800 md:text-xl text-xl">
+              <h3 class="font-black font-Authorfont tracking-wider mb-5 text-gray-100 md:text-xl text-xl">
                 -By A.M. Gautam
               </h3>
               <span
@@ -22,7 +22,7 @@ const HomeHeroCards = ({ imageSrc, backgroundColor }) => {
               >
                 Read Now
               </span>
-              <p class="md:text-lg text-gray-800 text-base max-sm:text-[10px]">
+              <p class="md:text-lg text-gray-200 text-base max-sm:text-[10px]">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Officiis. Lorem ipsum dolor sit amet consectetur adipisicing
                 elit. Officiis.
@@ -30,16 +30,14 @@ const HomeHeroCards = ({ imageSrc, backgroundColor }) => {
             </div>
           </div>
         </div>
-        <div className="transition-all ease-in-out duration-200 absolute bg-gray-800 top-0 right-0 w-[55%] h-full">
-          <div className="w-full h-full flex justify-end items-center">
-            <img
-              src={imageSrc}
-              className="h-[300px] 2xl:h-[350px] max-xl:h-[280px] max-lg:h-[250px] max-md:h-[200px] max-sm:h-[170px] mr-10 max-md:mr-7 max-sm:mr-2"
-              alt=""
-            />
-          </div>
+        <div className="transition-all ease-in-out duration-200 absolute bg-gray-200 top-0 right-0 w-[55%] h-full">
+          <div
+            className="w-full h-full flex justify-end items-center inside-shadow  bg-no-repeat bg object-center -z-50 bg-cover"
+            style={{
+              backgroundImage: `url(${story?.cover2})`,
+            }}
+          ></div>
         </div>
-        {/* <div className="w-full absolute h-full bg-[rgba(0,0,0,.4)] z-[9999] "></div> */}
       </div>
     </div>
   );
--- src/section/NewsEvents/Hero.jsx
+++ src/section/NewsEvents/Hero.jsx
@@ -64,39 +64,6 @@ export default function Hero() {
                   />
                 </div>
               </div>
-              <div class="hidden justify-between item-center absolute bottom-0 ">
-                <div class="flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 text-yellow-500"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
-                  <p class="text-gray-200 font-medium max-sm:text-[10px] md:block mr-10 max-sm:mr-3">
-                    Ratings
-                  </p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 text-pink-500"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  <p class="text-gray-200 max-sm:text-[10px]  font-bold text-sm">
-                    4.96
-                    <span class="text-gray-200 max-sm:text-[10px]  font-normal">
-                      (76 reviews)
-                    </span>
-                  </p>
-                </div>
-              </div>
             </div>
           </div>
         </div>
