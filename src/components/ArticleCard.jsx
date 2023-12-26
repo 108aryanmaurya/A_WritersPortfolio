ff8d615c2cbc994cc3e2d4f3bc2e2de3e17ca09f
Aryan <108aryanmaurya@gmail.com>
2023-12-26 13:05:38 +0530

    progress on testimonials

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -8,13 +8,13 @@ const ArticleCard = ({ art }) => {
   return (
     <div key={2} className="max-md:flex-col flex w-full">
       <div
-        className="max-md:w-full mr-10"
+        className="max-md:w-full max-md:mr-0 mr-10"
         onClick={() => {
           navigate(`/Article/${art?.title.replace(/\s+/g, "-")}`);
         }}
       >
         <span
-          className="text-[30px] max-md:block hidden max-md:text-[22px] underline  uppercase text-gray-800 font-Oswald max-md:leading-7 max-md:mb-5 "
+          className="text-[30px] max-md:block hidden max-md:text-[21px] underline  uppercase text-gray-800 font-Oswald max-md:leading-7 max-md:mb-4 "
           onClick={() => {
             navigate(`/Article/${art?.title.replace(/\s+/g, "-")}`);
           }}
@@ -24,7 +24,7 @@ const ArticleCard = ({ art }) => {
         <img src={art.cover} alt="" className="cursor-pointer w-[650px]" />
       </div>
       <div className="w-[60%] max-md:w-full flex max-md:gap-0  flex-col gap-4">
-        <div className="max-md:mt-2">
+        <div className="max-md:mt-2 max-md:text-[16px]">
           <span>Published in </span>
           <a href={art.link} target="_blank" rel="noopener noreferrer">
             {" "}
@@ -42,7 +42,7 @@ const ArticleCard = ({ art }) => {
           {art?.title}
         </span>{" "}
         <span
-          className="max-md:leading-6 leading-7 max-md:mt-3  text-[18px] 2xl:text-[22px] text-justify max-md:text-[21px] font-TimesNewRoman "
+          className=" leading-7 max-md:mt-3  text-[18px] 2xl:text-[22px] text-justify max-md:text-[18px] max-md:leading-5 font-TimesNewRoman "
           onClick={() => {
             navigate(`/Article/${art?.title.replace(/\s+/g, "-")}`);
           }}
